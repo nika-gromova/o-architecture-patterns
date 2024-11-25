6c4afa1f61a775e6c0c9de63a85f276dd031ee1b
Veronika Gromova <nicky.gromova@gmail.com>
2024-11-25 23:03:48 +0300

    [hw-3] register handlers without counters

--- game/errors/handler_test.go
+++ game/errors/handler_test.go
@@ -151,3 +151,151 @@ func TestHandler_Handle(t *testing.T) {
 
 	})
 }
+
+func TestHandler_Handle2(t *testing.T) {
+	var (
+		queue         = make(chan base.Command)
+		moveCommand   = &move.MoveCommand{}
+		rotateCommand = &rotate.RotateCommand{}
+		testHandler   = NewHandler()
+	)
+
+	testHandler.Register("MoveCommand", base.ErrGetProperty.Error(), func(command base.Command, err error) base.Command {
+		return &commands.QueueCommand{
+			Queue: queue,
+			Cmd: &commands.LogCommand{
+				Err: base.ErrSetProperty,
+				Cmd: moveCommand,
+			},
+		}
+	})
+
+	testHandler.Register("MoveCommand", base.ErrSetProperty.Error(), func(command base.Command, _ error) base.Command {
+		return &commands.QueueCommand{
+			Queue: queue,
+			Cmd: &commands.RepeatCommand{
+				Cmd: command,
+			},
+		}
+	})
+
+	testHandler.Register("RotateCommand", base.ErrGetProperty.Error(), func(command base.Command, _ error) base.Command {
+		return &commands.RepeatCommand{
+			Cmd: command,
+		}
+	})
+	testHandler.Register("RepeatCommand", base.ErrGetProperty.Error(), func(command base.Command, err error) base.Command {
+		return &commands.LogCommand{
+			Cmd: command,
+			Err: err,
+		}
+	})
+
+	testHandler.Register("ErrorCommand", testError.Error(), func(command base.Command, _ error) base.Command {
+		return &commands.RetryCommand{
+			Cmd: command,
+		}
+	})
+	testHandler.Register("RetryCommand", testError.Error(), func(command base.Command, _ error) base.Command {
+		return &commands.RepeatCommand{
+			Cmd: command,
+		}
+	})
+	testHandler.Register("RepeatCommand", testError.Error(), func(command base.Command, err error) base.Command {
+		return &commands.LogCommand{
+			Cmd: command,
+			Err: err,
+		}
+	})
+
+	t.Run("возвращает команду, которая ставит в очередь логирующую команду", func(t *testing.T) {
+		cmd := moveCommand
+		err := base.ErrGetProperty
+		expected := &commands.QueueCommand{
+			Queue: queue,
+			Cmd: &commands.LogCommand{
+				Err: base.ErrSetProperty,
+				Cmd: moveCommand,
+			},
+		}
+
+		got := testHandler.Handle(cmd, err)
+
+		require.Equal(t, expected, got)
+	})
+
+	t.Run("возвращает команду, которая ставит в очередь повторяющую команду", func(t *testing.T) {
+		cmd := moveCommand
+		err := base.ErrSetProperty
+		expected := &commands.QueueCommand{
+			Queue: queue,
+			Cmd: &commands.RepeatCommand{
+				Cmd: moveCommand,
+			},
+		}
+
+		got := testHandler.Handle(cmd, err)
+
+		require.Equal(t, expected, got)
+	})
+
+	t.Run("возвращает дефолтную команду, если обработчик не был найден", func(t *testing.T) {
+		cmd := rotateCommand
+		err := fmt.Errorf("unknown error")
+		expected := &commands.LogCommand{
+			Err: err,
+			Cmd: rotateCommand,
+		}
+
+		got := testHandler.Handle(cmd, err)
+
+		require.Equal(t, expected, got)
+	})
+
+	t.Run("на первый вызов команда добавляется повторяющая команда, на второй - логирующая", func(t *testing.T) {
+		cmd := rotateCommand
+		err := base.ErrGetProperty
+		expected1 := &commands.RepeatCommand{
+			Cmd: rotateCommand,
+		}
+		expected2 := &commands.LogCommand{
+			Err: err,
+			Cmd: expected1,
+		}
+
+		got1 := testHandler.Handle(cmd, err)
+
+		require.Equal(t, expected1, got1)
+
+		got2 := testHandler.Handle(got1, err)
+
+		require.Equal(t, expected2, got2)
+	})
+
+	t.Run("на первые 2 вызова возвращается повторяющая команда, на третий - ретрай команда", func(t *testing.T) {
+		cmd := &ErrorCommand{}
+		err := testError
+		expected1 := &commands.RetryCommand{
+			Cmd: cmd,
+		}
+		expected2 := &commands.RepeatCommand{
+			Cmd: expected1,
+		}
+		expected3 := &commands.LogCommand{
+			Err: err,
+			Cmd: expected2,
+		}
+
+		got1 := testHandler.Handle(cmd, err)
+
+		require.Equal(t, expected1, got1)
+
+		got2 := testHandler.Handle(got1, err)
+
+		require.Equal(t, expected2, got2)
+
+		got3 := testHandler.Handle(got2, err)
+
+		require.Equal(t, expected3, got3)
+	})
+}
